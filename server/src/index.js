01b3cf32af1654e92a6242c0bd83229b91c9e6fa
romii225 <romiicastro225@gmail.com>
2026-06-06 00:31:06 -0300

    feat: configurar CORS y variables de entorno

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 dotenv.config();
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 connectDB();
 
