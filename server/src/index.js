106a6d18c0f161bb1562ebc9f62d0ea36688545f
romii225 <romiicastro225@gmail.com>
2026-06-06 12:45:00 -0300

    fix: actualizar configiracion de cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors(corsOptions));
+
 
 connectDB();
 
@@ -21,4 +28,4 @@ app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
-module.exports = app;
+export default app
