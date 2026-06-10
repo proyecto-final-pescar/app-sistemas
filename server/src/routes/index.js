8bed3cf82a419776c540c3a84e83da22dec78da6
romii225 <romiicastro225@gmail.com>
2026-06-09 23:55:57 -0300

    Endpoint POST de registro

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,1 +1,9 @@
 // Routes: rutas agrupadas por recurso
+import { Router } from 'express';
+import { register } from '../controllers/index.js';
+
+const router = Router();
+
+router.post('/auth/register', register);
+
+export default router;
