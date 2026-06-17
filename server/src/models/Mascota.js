3d04c484223ad70a499460b2d8ef8e7958a433c6
Romina Castro <romiicastro225@gmail.com>
2026-06-17 12:56:55 -0300

    Agregue required en todos los campos

--- server/src/models/Mascota.js
+++ server/src/models/Mascota.js
@@ -14,20 +14,25 @@ const mascotaSchema = new mongoose.Schema(
     },
     raza: {
       type: String,
+      required: [true, 'La raza es requerida'],
       trim: true
     },
     fechaNacimiento: {
-      type: Date
+      type: Date,
+      required: [true, 'La fecha de nacimiento es requerida']
     },
     foto: {
-      type: String
+      type: String,
+      required: [true, 'La foto es requerida']
     },
     esCastrado: {
       type: Boolean,
+      required: [true, 'El campo castrado es requerido'],
       default: false
     },
     peso: {
-      type: Number
+      type: Number,
+      required: [true, 'El peso es requerido']
     },
     dueñoId: {
       type: mongoose.Schema.Types.ObjectId,
