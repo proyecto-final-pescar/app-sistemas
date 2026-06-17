208ed1b9d78e3d522110608e3cf471f99794392b
Angeles Piris <piris.angeles.imp@gmail.com>
2026-06-17 19:52:44 -0300

    Importación del middleware de autenticación auth.js y el modelo de mascota Mascota.js

--- server/src/routes/mascotas.js
+++ server/src/routes/mascotas.js
@@ -1,2 +1,8 @@
 const express = require('express'); // Importé el framework Express para manejar el servidor
 const router = express.Router(); // Creé un enrutador para las rutas de mascotas
+
+// 1. Importar el middleware de autenticación
+const authMiddleware = require('../middleware/auth');
+
+// 2. Importar el modelo de Mascota
+const Mascota = require('../models/Mascota');
