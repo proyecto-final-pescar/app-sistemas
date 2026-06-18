cd18a9785f48e827ca7bd6776a821da0b99e475f
Angeles Piris <piris.angeles.imp@gmail.com>
2026-06-18 00:02:40 -0300

    Creación del endpoint PUT /mascotas/:id que actualiza los datos de una mascota verificando con req.user.id que pertenezca al usuario autenticado antes de permitir la edición, devolviendo 403 si no le corresponde. Atrapando los posibles errores poe un dato inválido o también por un id inválido.

--- server/src/routes/mascotas.js
+++ server/src/routes/mascotas.js
@@ -20,7 +20,7 @@ router.get('/', authMiddleware, async (req, res) => {
 // POST /mascotas: Crea una nueva mascota
 router.post('/', authMiddleware, async (req, res) => {
     try {
-        const dueñoId = req.user.id; // Obtenemos el ID del dueño desde el token de autenticación
+        const usuarioId = req.user.id; // Obtengo el ID del dueño desde el token de autenticación
 
         const { nombre, especie, raza, fechaNacimiento, foto, esCastrado, peso } = req.body; // Extraigo las propiedades que necesito del body
 
@@ -33,7 +33,7 @@ router.post('/', authMiddleware, async (req, res) => {
             foto,
             esCastrado,
             peso,
-            dueñoId
+            usuarioId
         });
 
         // Guardo la mascota en la base de datos
@@ -50,6 +50,48 @@ router.post('/', authMiddleware, async (req, res) => {
         console.error("Error en POST /mascotas:", error);
         // Error general del servidor
         res.status(500).json({ mensaje: "Hubo un error al crear la mascota" });
+    }
+});
+
+// PUT /mascotas: Actualiza una mascota existente
+router.put('/:id', authMiddleware, async (req, res) => {
+    try {
+        const mascotaId = req.params.id; // El ID de la mascota que viene en la URL (ej: /mascotas/64f1a...)
+        const usuarioId = req.user.id; // Obtengo el ID del dueño desde el token de autenticación
+
+        const mascota = await Mascota.findById(mascotaId); // Busco la mascota en la base de datos por ID
+
+        // Si la mascota no existe o no es encontrada:
+        if (!mascota) {
+            return res.status(404).json({ mensaje: 'Mascota no encontrada' });
+        }
+
+        // Si la mascota solicitada no pertenece al usuario:
+        if (mascota.dueñoId.toString() !== usuarioId) {
+            return res.status(403).json({ mensaje: 'No tenés permiso para editar esta mascota' });
+        }
+
+        // Actualización de datos
+        const camposPermitidos = ['nombre', 'especie', 'raza', 'fechaNacimiento', 'foto', 'esCastrado', 'peso']; // Lista de los campos que el cliente puede modificar; evitando la posibilidad de que modifique algo 'peligroso' como dueñoId
+
+        camposPermitidos.forEach((campo) => {
+            if (req.body[campo] !== undefined) {
+                mascota[campo] = req.body[campo];
+            }
+        }); // Recorro los campos de la lista para encontrar aquel que el usuario quiera modificar y lo actualizo al nuevo valor.
+        
+        const mascotaActualizada = await mascota.save(); // Guardo los cambios en la base de datos
+        res.json(mascotaActualizada); // Devuelvo al cliente la mascota ya actualizada.
+
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            return res.status(400).json({ mensaje: 'Datos inválidos', error: error.message });
+        }
+        if (error.name === 'CastError') {
+            return res.status(400).json({ mensaje: 'El id de la mascota no es válido' });
+        }
+        console.error('Error en PUT /mascotas', error);
+        res.status(500).json({ mensaje: 'Error al actualizar la mascota' });
     }
 });
 
