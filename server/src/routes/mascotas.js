06afe5209282eb7016e3740cd36b79a1935b71c5
Angeles Piris <piris.angeles.imp@gmail.com>
2026-06-17 21:47:10 -0300

    Creación del endpoint GET /mascotas que devuelve la mascota del usuario usando req.user.id para que no se devuelvan mascotas que no correspondan con el usuario

--- server/src/routes/mascotas.js
+++ server/src/routes/mascotas.js
@@ -6,3 +6,17 @@ const authMiddleware = require('../middleware/auth');
 
 // 2. Importar el modelo de Mascota
 const Mascota = require('../models/Mascota');
+
+// GET /mascotas: devuelve las mascotas del usuario logueado
+router.get('/', authMiddleware, async (req, res) => {
+    try {
+        const usuarioId = req.user.id;
+        const mascotas = await Mascota.find({ dueñoId: usuarioId });
+        res.json(mascotas);
+    } catch (error) {
+        console.error('Error en GET /mascotas:', error);
+        res.status(500).json({ mensaje: 'Error al obtener las mascotas'});
+    }
+});
+
+module.exports = router;
