e8afe8695b416586506df4c0526102dd1d22eb19
Romina Castro <romiicastro225@gmail.com>
2026-06-28 18:20:04 -0300

    Agregar integracion con Google Places API y hacer vistas de Registro de Veterinaria

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -6,6 +6,7 @@ import authRoutes from './rutasRegistro.js';
 import mascotasRouter from './mascotas.js';
 import usuariosRouter from './usuarios.js';
 import veterinariasRouter from './veterinarias.js';
+import placesRouter from './places.js';
 
 const router = Router()
 
@@ -18,6 +19,6 @@ router.use('/mascotas', mascotasRouter);
 router.use('/usuarios', usuariosRouter);
 
 router.use('/veterinarias', veterinariasRouter);
-
+router.use('/places', placesRouter);
 
 export default router;
