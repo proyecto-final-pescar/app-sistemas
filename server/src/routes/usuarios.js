469c054873dbb09f01327c78f6b2b6c00b642a16
Gaston Vilte <gastonracing98@gmail.com>
2026-07-23 01:53:38 -0300

    feat: se agrega ABMD del user - admin

--- server/src/routes/usuarios.js
+++ server/src/routes/usuarios.js
@@ -1,9 +1,22 @@
 import { Router } from 'express'
 import verifyToken from '../middleware/auth.js'
-import { obtenerPerfilUsuario } from '../controllers/userController.js'
+import verificarRol from '../middleware/roles.js'
+import { 
+    obtenerPerfilUsuario, 
+    crearUsuarioAdmin, 
+    darDeBajaUsuario,
+    actualizarUsuarioAdmin
+} from '../controllers/userController.js'
 
 const router = Router()
 
 router.get('/:id', verifyToken, obtenerPerfilUsuario)
+
+router.post('/admin', verifyToken, crearUsuarioAdmin)
+
+// 3. Baja de usuario (Soft Delete - Exclusivo para el Administrador)
+router.delete('/:id', verifyToken, darDeBajaUsuario)
+
+router.put('/:id', verifyToken, verificarRol('administrador'), actualizarUsuarioAdmin)
 
 export default router
