7e7b832a233a0991e5b8e2e319451a49a9f6d123
lujan2281-ux <lujan2281@gmail.com>
2026-08-03 15:05:49 -0300

    feat(S11-01): configura SDK de Mercado Pago y variables de entorno

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 
 import connectDB from "./config/db.js";
 import routes from "./routes/index.js";
+import "./config/mercadopago.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import { iniciarJobsTurnos } from "./jobs/turnoJobs.js";
 
